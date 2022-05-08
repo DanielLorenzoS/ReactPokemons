72fa7a55c6f56855f30505bcb47f0d60dc8974b4
DanielLorenzoS <daniellorenzo47@aragon.unam.mx>
2022-05-08 00:07:44 -0500

    Netlify

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
-import { DataProvider } from "./context/DataContext";
-import { RemoveDataProvider } from "./context/RemoveContext";
 import Data from "./views/Data";
 import Favorites from "./views/Favorites";
 
 function App() {
   return (
     <> 
-        {/* <Header />
-      <DataProvider>
-        <RemoveDataProvider>
-          <Router>
-            <Routes>
-              <Route path="/" element={<Data />}></Route>
-              <Route path="/favorites" element={<Favorites />} />
-            </Routes>
-          </Router>
-        </RemoveDataProvider>
-      </DataProvider> */}
           <Header/>
         <Routes>
           <Route path="/data" element={<Data/>}></Route>
